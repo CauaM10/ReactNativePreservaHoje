6c50070ac63126f55be047894f3f34570156b92d
PedrooL77 <pedrosenai777@gamail.com PedrooL77>
2024-09-11 12:12:12 -0300

    Inicialização do Projeto

--- src/Routes/Rotas.js
+++ src/Routes/Rotas.js
@@ -6,9 +6,9 @@ import { useContext } from 'react';
 import { AuthContext } from '../Context/AuthContext';
 
 
-import Busca from '../Pages/Busca';
+
 import Login from '../Pages/Login';
-import Inserir from '../Pages/Inserir';
+
 
 
 const Tab = createBottomTabNavigator();
@@ -33,25 +33,7 @@ export default function Rotas() {
                     tabBarActiveTintColor: "white"
                 }}
             >
-                <Tab.Screen
-                    name="Busca"
-                    component={Busca}
-                    options={{
-                        tabBarIcon: ({ color, size }) => (
-                            <MaterialCommunityIcons name="magnify" color={color} size={size} />
-                        ),
-                    }}
-
-                />
-                <Tab.Screen
-                    name="Inserir"
-                    component={Inserir}
-                    options={{
-                        tabBarIcon: ({ color, size }) => (
-                            <MaterialCommunityIcons name="plus-box" color={color} size={size} />
-                        ),
-                    }}
-                />
+    
             </Tab.Navigator>
         </NavigationContainer>
     )
